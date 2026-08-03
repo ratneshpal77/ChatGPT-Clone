a094dc45165d063a16dd5410d42ddb894e488f49
ratneshpal77 <ratneshpal776@gmail.com>
2026-08-04 01:58:26 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,100 @@
-import cors from "cors";
-import "dotenv/config";
+// import cors from "cors";
+// import "dotenv/config";
+// import express from "express";
+// import mongoose from "mongoose";
+// import chatRoutes from "./routes/chat.js";
+// import morgan from "morgan";
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+// app.use(morgan("dev")); // It is inbuild express middleware
+
+// app.get("/server", (req, res) => {
+//   res.send("Server is working");
+// });
+
+// app.get("/db-test", async (req, res) => {
+//   try {
+//     res.json({
+//       readyState: mongoose.connection.readyState,
+//       host: mongoose.connection.host,
+//       db: mongoose.connection.name,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// app.use("/api", chatRoutes);
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+
+//     console.log("✅ MongoDB Connected");
+//   } catch (err) {
+//     console.error("❌ MongoDB Error:", err);
+//   }
+// };
+
+// console.log(process.env.MONGODB_URI);
+
+// const PORT = process.env.PORT || 8080;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+//   connectDB();
+// });
+
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
-import chatRoutes from "./routes/chat.js";
 import morgan from "morgan";
+import "dotenv/config";
+
+import chatRoutes from "./routes/chat.js";
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
-app.use(morgan("dev")); // It is inbuild express middleware
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/server", (req, res) => {
   res.send("Server is working");
 });
 
+app.get("/db-test", async (req, res) => {
+  try {
+    res.json({
+      readyState: mongoose.connection.readyState,
+      host: mongoose.connection.host,
+      db: mongoose.connection.name,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 app.use("/api", chatRoutes);
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
+let isConnected = false;
 
-    console.log("✅ MongoDB Connected");
-  } catch (err) {
-    console.error("❌ MongoDB Error:", err);
-  }
-};
+async function connectDB() {
+  if (isConnected) return;
 
-console.log(process.env.MONGODB_URI);
+  await mongoose.connect(process.env.MONGODB_URI);
 
-const PORT = process.env.PORT || 8080;
+  isConnected = true;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  connectDB();
+  console.log("Mongo Connected");
+}
+
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
 });
+
+export default app;
