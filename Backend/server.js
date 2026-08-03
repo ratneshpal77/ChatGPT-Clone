2be6e53f12a7f6663fd264467edcec3471dfbf93
ratneshpal77 <ratneshpal776@gmail.com>
2026-08-03 13:43:18 +0530

    Fix server issue

--- Backend/server.js
+++ Backend/server.js
@@ -1,67 +1,25 @@
-// import cors from "cors";
-// import "dotenv/config";
-// import express from "express";
-// import mongoose from "mongoose";
-// import chatRoutes from "./routes/chat.js";
-
-
-// const app = express();
-// const PORT = 8080;
-
-// app.use(express.json());
-// app.use(cors());
-
-// app.use("/api", chatRoutes);
-
-
-
-// app.listen(PORT, () => {
-//   console.log(`server running on ${PORT}`);
-//   connectDB();
-// });
-
-// const connectDB = async() => {
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URI);
-//         console.log("Connected with Database!");
-//     } catch(err) {
-//         console.log("Failed to connect with Db", err);
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "*",   // abhi sab allow
-}));
+app.use(
+  cors({
+    origin: "*", // abhi sab allow
+  }),
+);
+app.use(morgan("dev")); // It is inbuild express middleware
+
+app.get("/server", (req, res) => {
+  res.send("Server is working");
+});
+
+
 
 app.use("/api", chatRoutes);
 
@@ -80,6 +38,3 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
 });
-
-
-
